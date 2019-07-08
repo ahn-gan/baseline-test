9ff713ae731ff00bd7758c0e762bad26815a2406
Ahn <gan_jinan@qq.com>
2019-07-08 11:56:55 +0800

    Update practice-4.js

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    var aLength = collectionA.length;
+    var bArray = objectB["value"];
+    var bLength = bArray.length;
+    var resultArray = [];
+    debugger
+    for (var i = 0; i < aLength; i++) {
+      for (var j = 0; j < bLength; j++) {
+        if(collectionA[i].key == bArray[j]) {
+          resultArray.push(collectionA[i].key);
+          break;
+        }
+      }
+    }
+    return resultArray;
 }
